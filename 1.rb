1106095307bf429eb2ffc23dc79cd12ae571d5a0
Earl Pilloud <earl.pilloud@gmail.com>
2012-06-09 15:38:33 -0600

    problem 1, summing multiples under 1000

---
+++ 1.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+
+def multiples_up_to(multiple, limit)
+  (1..limit - 1).select { |x| x % multiple == 0}
+end
+
+threes = multiples_up_to(3, 1000)
+fives = multiples_up_to(5, 1000)
+
+puts (threes | fives).inject(:+)
