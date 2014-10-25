cec16dd2768e45276d3f9ce2ef7758e0bfa59e0a
Colin Pilloud <colin.pilloud@gmail.com>
2014-10-25 09:45:26 -0600

    Add timing to driver script

--- driver.rb
+++ driver.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
 
 Dir.glob("*/*.rb").each do |problem|
-  print "#{problem}: ", `#{problem} 2> /dev/null`
+  start = Time.now
+  solution = `#{problem} 2> /dev/null`.chomp
+  execution_time = Time.now - start
+  puts "#{problem}: #{solution}, calculated in #{execution_time}"
 end
