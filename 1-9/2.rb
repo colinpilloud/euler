9a8d84f6504a3c8b1e882931884a53f1c3c73ff1
Colin Pilloud <colin.pilloud@gmail.com>
2014-10-26 13:23:50 -0600

    Refactor problem 2, using simplified formula for finding even fibonacci numbers

--- 1-9/2.rb
+++ 1-9/2.rb
@@ -2,22 +2,14 @@
 
 class Solution2
   def initialize
-    @fibo_map = { 1 => 1, 2 => 2 }
+    @fibo_map = { 1 => 2, 2 => 8 }
   end
 
-  def fibonacci(n)
-    unless @fibo_map.has_key?(n)
-      @fibo_map[n] = fibonacci(n-1) + fibonacci(n-2)
-    end
-    @fibo_map[n]
+  def even_fibonacci(n)
+    return @fibo_map[n] if @fibo_map.has_key? n
+    @fibo_map[n] = 4 * even_fibonacci(n-1) + even_fibonacci(n-2)
   end
 end
 
 s = Solution2.new
-# 35 is known to contain the limit for the problem (4 million)
-# Clever way to calculate?
-fibo_seq = (1..35).collect {|i| s.fibonacci(i) }
-# Grab evens less than 4 million
-even_fibo = fibo_seq.select { |fibo| fibo < 4000000 and fibo % 2 == 0 }
-# Sum over even fibonaccis under 4 million
-puts even_fibo.inject(:+)
+puts (1..12).map { |i| s.even_fibonacci(i) }.select { |f| f < 4e6.to_i }.inject(:+)
