4db4a101dc6dbc88c44002315ddfcdda4ecce570
Colin Pilloud <colin.pilloud@gmail.com>
2014-11-03 12:49:43 -0700

    test only primes less than 1000 for speed increase

--- 20-29/26.rb
+++ 20-29/26.rb
@@ -1,11 +1,12 @@
 #!/usr/bin/env ruby
 
 require "bigdecimal"
+require "prime"
 
 longest_cycle = 0
 target = 0
 
-2.upto(1000).each do |i|
+Prime.each(1000) do |i|
   s = (BigDecimal.new(1).div(BigDecimal.new(i), 2000)).to_s.gsub(/E\d+$/, "").chars.join(" ")
   match = s.match(/\b(\d+(:? \d+\b)+?)(?: \1)+\b/)
 
