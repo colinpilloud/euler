c420485a4696c9daf2408484703c6cc51f395f0f
Colin Pilloud <colin.pilloud@gmail.com>
2014-11-10 20:40:29 -0700

    simplifying problem 34, establishing upper bound

--- 30-39/34.rb
+++ 30-39/34.rb
@@ -1,21 +1,10 @@
 #!/usr/bin/env ruby
 
-class Solution34
-  def initialize
-    @factorials = [1, 1, 2]
-  end
+@factorials = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880]
 
-  def fact(n)
-    return @factorials[n] if @factorials[n]
-    @factorials[n] = fact(n - 1) * n
-  end
-
-  def is_sum_of_digit_factorials?(n)
-    n == n.to_s.chars.map(&:to_i).inject(0) {|r,d| r += fact(d) }
-  end
+def is_sum_of_digit_factorials?(n)
+  n == n.to_s.chars.map(&:to_i).inject(0) {|r,d| r += @factorials[d] }
 end
 
-s34 = Solution34.new
-s34.fact(9) # populate fact map
-
-puts (3..1e7).select {|i| s34.is_sum_of_digit_factorials? i }.inject(:+)
+MAX = 1854721 # http://en.wikipedia.org/wiki/Factorion#Upper_bound
+puts (3..MAX).select {|i| is_sum_of_digit_factorials? i }.inject(:+)
