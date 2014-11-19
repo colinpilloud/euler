2283dc4fac99ddc9ac115001ea5bae2740daa881
Colin Pilloud <colin.pilloud@gmail.com>
2014-11-19 08:00:40 -0700

    add another loop, change loops to iterate over 3 and 2 digit sets, and 1 and 4 digit sets respectively

--- 30-39/32.rb
+++ 30-39/32.rb
@@ -8,13 +8,19 @@ def is_pandigital?(n)
   n.to_s.chars.sort.join.eql? "123456789"
 end
 
-def is_product_pandigital(n, m)
+def is_product_pandigital?(n, m)
   is_pandigital?(n.to_s + m.to_s + (n*m).to_s)
 end
 
-1.upto(10000).each do |a|
-  1.upto(10000).each do |b|
-    pandigital_numbers << (a*b) if is_product_pandigital(a, b)
+100.upto(999).each do |a|
+  10.upto(99).each do |b|
+    pandigital_numbers << (a*b) if is_product_pandigital?(a, b)
+  end
+end
+
+1000.upto(9999).each do |a|
+  1.upto(9).each do |b|
+    pandigital_numbers << (a*b) if is_product_pandigital?(a, b)
   end
 end
 
